481d2744791c96fad8960003bf685f206c239ce0
Adrian Alexander <adrianmauricio337@gmail.com>
2021-05-17 13:46:06 -0500

    El primer commit de estos apuntes

---
+++ opp.py
@@ -0,0 +1,6 @@
+# self hace referencia al objeto (this en python)
+# class <nombre_de_la_clase> (super_clase):
+#    def __init__ (self, params): constructor de la clase
+#       self.params = params
+#    def <nombre_del_metodo> (self,<parametros>):
+#        <params>
